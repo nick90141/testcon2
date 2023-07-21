be73298218bfb8c7a258d96a61f4ea2068e51142
nick90141 <139743447+nick90141@users.noreply.github.com>
2023-07-21 11:46:50 +0300

    Third commit from remote

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First message");
         System.out.println("Second message");
+        System.out.println("Third message");
+
 
     }
 }
